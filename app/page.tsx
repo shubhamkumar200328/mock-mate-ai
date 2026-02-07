ad2aed6f75d54d0b4229f3b65ac1e8b31e32f796
Shubham Kumar <shubhamkumar200328@gmail.com>
2026-02-07 10:53:44 +0530

    project setup and some initial files for auth and root

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <div className="">
+        <h1>MockMateAI</h1>
+        <p className="">Hi, you have grand welcome here.</p>
+      </div>
+    </div>
+  );
+}
